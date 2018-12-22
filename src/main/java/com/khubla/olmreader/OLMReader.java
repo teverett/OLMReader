db4a70224ce239ac6a59deec9de2b80d1cc5b4d8
Tom Everett <tom@khubla.com>
2018-12-22 14:39:49 -0700

    whups

--- src/main/java/com/khubla/olmreader/OLMReader.java
+++ src/main/java/com/khubla/olmreader/OLMReader.java
@@ -30,21 +30,21 @@ public class OLMReader {
       CommandLine cmd = null;
       try {
          cmd = parser.parse(options, args);
+         /*
+          * get the file
+          */
+         final String filename = cmd.getOptionValue(FILE_OPTION);
+         if (null != filename) {
+            /*
+             * read to console
+             */
+            final OLMReaderConsoleReader olmReaderConsoleReader = new OLMReaderConsoleReader();
+            olmReaderConsoleReader.read(filename);
+         }
       } catch (final Exception e) {
          e.printStackTrace();
          final HelpFormatter formatter = new HelpFormatter();
          formatter.printHelp("posix", options);
-      }
-      /*
-       * get the file
-       */
-      final String filename = cmd.getOptionValue(FILE_OPTION);
-      if (null != filename) {
-         /*
-          * read to console
-          */
-         final OLMReaderConsoleReader olmReaderConsoleReader = new OLMReaderConsoleReader();
-         olmReaderConsoleReader.read(filename);
       }
    }
 }
