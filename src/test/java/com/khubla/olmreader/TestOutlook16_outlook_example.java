92309b8620f7ea8724c577a212bb5936d436cfc7
Tom Everett <tom@khubla.com>
2020-05-20 16:23:39 -0600

    switch to junit4

--- src/test/java/com/khubla/olmreader/TestOutlook16_outlook_example.java
+++ src/test/java/com/khubla/olmreader/TestOutlook16_outlook_example.java
@@ -6,72 +6,69 @@
  */
 package com.khubla.olmreader;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.*;
 
-import com.khubla.olmreader.olm.OLMFile;
-import com.khubla.olmreader.olm.OLMMessageCallback;
-import com.khubla.olmreader.olm.OLMRawMessageCallback;
-import com.khubla.olmreader.olm.generated.Appointments.Appointment;
-import com.khubla.olmreader.olm.generated.Categories;
-import com.khubla.olmreader.olm.generated.Categories.Category;
-import com.khubla.olmreader.olm.generated.Contacts.Contact;
-import com.khubla.olmreader.olm.generated.Emails.Email;
-import com.khubla.olmreader.olm.generated.Groups.Group;
-import com.khubla.olmreader.olm.generated.Notes.Note;
-import com.khubla.olmreader.olm.generated.Tasks.Task;
+import com.khubla.olmreader.olm.*;
+import com.khubla.olmreader.olm.generated.*;
+import com.khubla.olmreader.olm.generated.Appointments.*;
+import com.khubla.olmreader.olm.generated.Categories.*;
+import com.khubla.olmreader.olm.generated.Contacts.*;
+import com.khubla.olmreader.olm.generated.Emails.*;
+import com.khubla.olmreader.olm.generated.Groups.*;
+import com.khubla.olmreader.olm.generated.Notes.*;
+import com.khubla.olmreader.olm.generated.Tasks.*;
 
 public class TestOutlook16_outlook_example implements OLMMessageCallback, OLMRawMessageCallback {
-   @Override
-   public void appointment(Appointment appointment) {
-      System.out.println("Appointment: " + appointment.getOPFCalendarEventCopyUUID().getValue());
-   }
+	@Override
+	public void appointment(Appointment appointment) {
+		System.out.println("Appointment: " + appointment.getOPFCalendarEventCopyUUID().getValue());
+	}
 
-   @Override
-   public void categories(Categories categories) {
-      for (final Category category : categories.getCategory()) {
-         System.out.println("Category: " + category.getOPFCategoryCopyName());
-      }
-   }
+	@Override
+	public void categories(Categories categories) {
+		for (final Category category : categories.getCategory()) {
+			System.out.println("Category: " + category.getOPFCategoryCopyName());
+		}
+	}
 
-   @Override
-   public void contact(Contact contact) {
-      System.out.println("Contact: " + contact.getOPFContactCopyDisplayName().getValue());
-   }
+	@Override
+	public void contact(Contact contact) {
+		System.out.println("Contact: " + contact.getOPFContactCopyDisplayName().getValue());
+	}
 
-   @Override
-   public void email(Email email) {
-      System.out.println("Email: " + email.getOPFMessageCopyMessageID().getValue());
-   }
+	@Override
+	public void email(Email email) {
+		System.out.println("Email: " + email.getOPFMessageCopyMessageID().getValue());
+	}
 
-   @Override
-   public void group(Group group) {
-      System.out.println("Group: " + group.getOPFGroupCopyDisplayName().getValue());
-   }
+	@Override
+	public void group(Group group) {
+		System.out.println("Group: " + group.getOPFGroupCopyDisplayName().getValue());
+	}
 
-   @Override
-   public void note(Note note) {
-      System.out.println("Note: " + note.getOPFNoteCopyTitle().getValue());
-   }
+	@Override
+	public void note(Note note) {
+		System.out.println("Note: " + note.getOPFNoteCopyTitle().getValue());
+	}
 
-   @Override
-   public void rawMessage(String olmMessage) {
-      // System.out.println(olmMessage);
-   }
+	@Override
+	public void rawMessage(String olmMessage) {
+		// System.out.println(olmMessage);
+	}
 
-   @Override
-   public void task(Task task) {
-      System.out.println("Task: " + task.getOPFTaskCopyName().getValue());
-   }
+	@Override
+	public void task(Task task) {
+		System.out.println("Task: " + task.getOPFTaskCopyName().getValue());
+	}
 
-   @Test
-   public void test_outlook_example() {
-      try {
-         final OLMFile olmFile = new OLMFile("src/test/resources/Outlook16/outlook_example.olm");
-         olmFile.readOLMFile(this, this);
-      } catch (final Exception e) {
-         e.printStackTrace();
-         Assert.fail();
-      }
-   }
+	@Test
+	public void test_outlook_example() {
+		try {
+			final OLMFile olmFile = new OLMFile("src/test/resources/Outlook16/outlook_example.olm");
+			olmFile.readOLMFile(this, this);
+		} catch (final Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
 }
